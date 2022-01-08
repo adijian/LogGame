e90a82046d61734446edb9f6f2e0b9b2db2b850d
adij <shoogeh.jian@gmail.com>
2022-01-08 19:45:58 +0200

    e

---
+++ src/game/GamePanel.java
@@ -0,0 +1,79 @@
+package game;
+
+import entity.Player;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+    public final int tileSize = originalTileSize * scale;
+    final int maxScreenColumn = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenColumn;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    Thread gameThread;
+    KeyHandler keyHandler = new KeyHandler();
+
+    int FPS = 144;
+
+    Player player = new Player(this, keyHandler);
+
+    GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void update(){
+        player.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        player.draw(g2);
+        g2.dispose();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = 1000000000d / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        int drawCount = 0;
+        int timer = 0;
+
+        while(gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if(timer >= 1000000000) {
+                System.out.println("FPS: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+}
---
+++ src/game/Main.java
@@ -0,0 +1,21 @@
+package game;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("WOW");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
