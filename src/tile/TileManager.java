d7adb7bdcf3599b7125ce6008c1c307d105f72a2
adij <shoogeh.jian@gmail.com>
2022-01-08 21:41:14 +0200

    tile map editor

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,17 +4,23 @@ import game.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Objects;
 
 public class TileManager {
     GamePanel gamePanel;
     Tile[] tile;
+    int mapTileNum[][];
 
     public TileManager(GamePanel gamePanel){
         this.gamePanel = gamePanel;
         tile = new Tile[10];
+        mapTileNum = new int[gamePanel.getMaxScreenColumn()][gamePanel.getMaxScreenRow()];
         getTileImage();
+        loadMap();
     }
 
     public void getTileImage() {
@@ -30,6 +36,37 @@ public class TileManager {
         }
     }
 
+    public void loadMap(){
+        try{
+            InputStream inputStream = getClass().getResourceAsStream("/maptiles/tileMap.txt");
+            assert inputStream != null;
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            int col = 0;
+            int row = 0;
+
+            while(col < gamePanel.getMaxScreenColumn() && row < gamePanel.getMaxScreenRow()) {
+                String line = bufferedReader.readLine();
+
+                while(col< gamePanel.getMaxScreenColumn()) {
+                    String[] numbers = line.split(" ");
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[col][row] = num;
+                    col++;
+                }
+                if (col == gamePanel.getMaxScreenColumn()) {
+                    col = 0;
+                    row ++;
+                }
+            }
+            bufferedReader.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     public void draw(Graphics2D g2) {
         int col = 0;
         int row = 0;
@@ -37,7 +74,10 @@ public class TileManager {
         int y = 0;
 
         while(col < gamePanel.getMaxScreenColumn() && row < gamePanel.getMaxScreenRow()) {
-            g2.drawImage(tile[0].image,x,y, gamePanel.tileSize, gamePanel.tileSize, null);
+
+            int tileNum = mapTileNum[col][row];
+
+            g2.drawImage(tile[tileNum].image,x,y, gamePanel.tileSize, gamePanel.tileSize, null);
             col++;
             x+= gamePanel.tileSize;
 
