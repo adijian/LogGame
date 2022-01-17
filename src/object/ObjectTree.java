0fc142d1014f2902f6a60e89ef934ad296755819
adijian <shoogeh.jian@gmail.com>
2022-01-17 23:10:13 +0200

    e

--- src/object/ObjectTree.java
+++ src/object/ObjectTree.java
@@ -21,12 +21,4 @@ public class ObjectTree extends SuperObject{
             e.printStackTrace();
         }
     }
-
-    public int getHp() {
-        return hp;
-    }
-
-    public void setHp(int hp) {
-        this.hp = hp;
-    }
 }
